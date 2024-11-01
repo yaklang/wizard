59c973884157a28d29ef7f1fdb93ecf2a5d448fe
whale <1745132665@qq.com>
2024-11-01 21:18:12 +0800

    wip: 配置环境变量

--- vite.config.ts
+++ vite.config.ts
@@ -1,35 +1,39 @@
 // vite.config.ts
 import UnoCSS from 'unocss/vite';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
-export default defineConfig({
-    plugins: [
-        react(),
-        UnoCSS({
-            configFile: './uno.config.ts',
-        }),
-    ],
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, 'src'),
-        },
-        extensions: ['.js', '.jsx', '.ts', '.tsx'],
-    },
-    server: {
-        // port: 8082,
-        proxy: {
-            '/api': {
-                // target: 'http://legion-4g.yaklang.com:8080/',
-                // 高鹏本地
-                target: 'http://192.168.3.3:8082/',
-                changeOrigin: true,
+export default defineConfig(({ mode, command }) => {
+    console.log(mode, process.cwd(), 'mode');
+    const env = loadEnv(mode, process.cwd());
+    return {
+        plugins: [
+            react(),
+            UnoCSS({
+                configFile: './uno.config.ts',
+            }),
+        ],
+        resolve: {
+            alias: {
+                '@': path.resolve(__dirname, 'src'),
             },
+            extensions: ['.js', '.jsx', '.ts', '.tsx'],
         },
-        host: '0.0.0.0',
-        hmr: true,
-    },
+        server: {
+            // port: 8082,
+            proxy: {
+                '/api': {
+                    // target: 'http://legion-4g.yaklang.com:8080/',
+                    // 高鹏本地
+                    target: env.VITE_BASE_URL,
+                    changeOrigin: true,
+                },
+            },
+            host: '0.0.0.0',
+            hmr: true,
+        },
+    };
 });
 
 // $r2}#TqJn$5dQYB]^0(J
