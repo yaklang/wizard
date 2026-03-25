e30dadd0500480b910b3602ef08e8680c6ede4fd
hubo <hubo@4dogs.cn>
2026-03-25 16:14:29 +0800

    fix： 修改主题色

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { useRoutes } from 'react-router-dom';
 import routers from './App/routers/routers';
 import irifyRouters from './App/routers/irify-routers';
-import '@yakit-libs/color/dist/index.css';
+// import '@yakit-libs/color/dist/index.css';
+import './styles/index.css';
 
 const APP_MODE = import.meta.env.VITE_APP_MODE;
 
