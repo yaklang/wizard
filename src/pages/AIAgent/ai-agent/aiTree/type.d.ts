880ef314f07e0b5f78873de2f2ced7417b10e7b8
hubo <hubo@4dogs.cn>
2026-03-30 14:22:58 +0800

    fix: 修改yakitTag组件关闭按钮

--- src/pages/AIAgent/ai-agent/aiTree/type.d.ts
+++ src/pages/AIAgent/ai-agent/aiTree/type.d.ts
@@ -1,26 +1,27 @@
-import type { AITaskInfoProps } from '@/pages/ai-re-act/hooks/aiRender';
+import type { AITaskInfoProps } from '@/pages/ai-re-act/hooks/aiRender'
 
 export interface AITreeProps {
-    tasks: AITaskInfoProps[];
-    onNodeClick?: AITreeNodeProps['onClick'];
+  tasks: AITaskInfoProps[]
+  onNodeClick?: AITreeNodeProps['onClick']
+  className?: string
 }
 
 export interface AITreeNodeProps {
-    order: number;
-    position: {
-        // 是否是起始节点
-        isStart: boolean;
-        // 是否是结束节点
-        isEnd: boolean;
-        // 是否是当前层级起始节点
-        isStartOfLevel: boolean;
-        // 是否是当前层级结束节点
-        isEndOfLevel: boolean;
-        // 是否是父级结束节点
-        isParentLast: boolean;
-        // 下一个层级差
-        levelDiff: number;
-    };
-    data: AITaskInfoProps;
-    onClick?: () => void;
+  order: number
+  position: {
+    // 是否是起始节点
+    isStart: boolean
+    // 是否是结束节点
+    isEnd: boolean
+    // 是否是当前层级起始节点
+    isStartOfLevel: boolean
+    // 是否是当前层级结束节点
+    isEndOfLevel: boolean
+    // 是否是父级结束节点
+    isParentLast: boolean
+    // 下一个层级差
+    levelDiff: number
+  }
+  data: AITaskInfoProps
+  onClick?: () => void
 }
