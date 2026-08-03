db6ccd6b5044a58d1acf6cf7cb83942316e3701b
whale <1745132665@qq.com>
2026-08-03 15:13:28 +0800

    fix: bugs

--- src/pages/TaskScript/taskScript/ModifyTaskScript.tsx
+++ src/pages/TaskScript/taskScript/ModifyTaskScript.tsx
@@ -53,7 +53,7 @@ const ModifyTaskScript: FC = () => {
     manual: true,
     onSuccess: async () => {
       message.success(state.type === 'add' ? '创建成功' : '编辑成功')
-      navigate('/task/create-task')
+      navigate('/system-management/task-script')
     },
     onError: (err) => {
       console.error(err)
